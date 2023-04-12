86b2bbfc2f6dfc9ec474d505dbead3a14b5ad1e3
Nasrulla Akhbulatov <nasrulla.akhbulatov@gmail.com>
2023-04-12 13:52:46 +0300

    Первый коммит

---
+++ app/src/main/java/com/akhbulatov/vcontachim/activity/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.akhbulatov.vcontachim.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import com.akhbulatov.vcontachim.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+}
