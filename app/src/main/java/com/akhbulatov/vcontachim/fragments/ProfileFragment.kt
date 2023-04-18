58b6dbc8cce0a7424602771f89c2303455b487e9
Nasrulla Akhbulatov <nasrulla.akhbulatov@gmail.com>
2023-04-18 17:34:55 +0300

    Сделал экран профиля

--- app/src/main/java/com/akhbulatov/vcontachim/fragments/ProfileFragment.kt
+++ app/src/main/java/com/akhbulatov/vcontachim/fragments/ProfileFragment.kt
@@ -1,7 +1,19 @@
 package com.akhbulatov.vcontachim.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.akhbulatov.vcontachim.R
+import com.akhbulatov.vcontachim.databinding.FragmentProfileBinding
 
-class ProfileFragment: Fragment(R.layout.fragment_profile) {
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+    private var binding: FragmentProfileBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentProfileBinding.bind(view)
+
+
+
+    }
 }
