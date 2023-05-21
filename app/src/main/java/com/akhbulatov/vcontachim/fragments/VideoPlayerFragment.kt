8ccebcee9efeb0c6a9ceef20178b7b7032ecbca5
Nasrulla Akhbulatov <nasrulla.akhbulatov@gmail.com>
2023-05-21 18:52:29 +0300

    Осуществил воспроизведение видео

--- app/src/main/java/com/akhbulatov/vcontachim/fragments/VideoPlayerFragment.kt
+++ app/src/main/java/com/akhbulatov/vcontachim/fragments/VideoPlayerFragment.kt
@@ -10,12 +10,16 @@ import com.akhbulatov.vcontachim.databinding.FragmentVideoPlayerBinding
 import com.akhbulatov.vcontachim.model.Video
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.dash.DashMediaSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import java.io.Serializable
 import java.text.SimpleDateFormat
 
 class VideoPlayerFragment : Fragment(R.layout.fragment_video_player) {
     private var binding: FragmentVideoPlayerBinding? = null
-    private var player: ExoPlayer? = null
+    private var exoPlayer: ExoPlayer? = null
+    private var playbackPosition = 0L
+    private var playWhenReady = true
 
     @SuppressLint("SimpleDateFormat", "SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -28,20 +32,12 @@ class VideoPlayerFragment : Fragment(R.layout.fragment_video_player) {
 
         val itemSerializable: Serializable = arguments?.getSerializable(ARGUMENTS_ITEM)!!
         val item: Video.Item = itemSerializable as Video.Item
-        binding!!.video.setOnClickListener {
-            player = ExoPlayer.Builder(requireContext())
-                .build()
-                .also { exoPlayer ->
-                    binding!!.video.player = exoPlayer
-                    val mediaItem = MediaItem.fromUri(item.player)
-                    exoPlayer.setMediaItem(mediaItem)
-                    exoPlayer.play()
-                }
-        }
+
         binding!!.title.text = item.title
         val formatter = SimpleDateFormat("d MMMM yyyy")
         val date = formatter.format(item.date * 1000)
         binding!!.date.text = date
+
         val plurals = VcontachimApplication.context.resources.getQuantityString(
             R.plurals.plurals_video,
             item.views
@@ -50,9 +46,55 @@ class VideoPlayerFragment : Fragment(R.layout.fragment_video_player) {
         binding!!.commentCount.text = item.comments.toString()
         binding!!.likesCount.text = item.likes.likes_count.toString()
         binding!!.shareCount.text = item.reposts.reposts_count.toString()
+
+        preparePlayer(item)
+    }
+
+    fun preparePlayer(item: Video.Item) {
+        exoPlayer = ExoPlayer.Builder(requireContext()).build()
+        exoPlayer?.playWhenReady = true
+        binding!!.video.player = exoPlayer
+        val defaultHttpDataSourceFactory = DefaultHttpDataSource.Factory()
+        val mediaItem = MediaItem.fromUri(URL)
+        val mediaSource =
+            DashMediaSource.Factory(defaultHttpDataSourceFactory).createMediaSource(mediaItem)
+        exoPlayer?.setMediaSource(mediaSource)
+        exoPlayer?.seekTo(playbackPosition)
+        exoPlayer?.playWhenReady = playWhenReady
+        exoPlayer?.pause()
+        exoPlayer?.prepare()
+
+    }
+
+    private fun releasePlayer() {
+        exoPlayer?.let { player ->
+            playbackPosition = player.currentPosition
+            playWhenReady = player.playWhenReady
+            player.release()
+            exoPlayer = null
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        releasePlayer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        releasePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        releasePlayer()
     }
 
     companion object {
+        const val URL2 =
+            "https://vk.com/video_ext.php?oid=-56643473&id=456245378&hash=b5e979a9793c2e6d&__ref=vk.api&api_hash=1684676843b4a0a093a97f38ef99_G44TKNJZGI4TGOI"
+        const val URL =
+            "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd"
         const val ARGUMENTS_ITEM = "ITEM"
 
         fun createVideoPlayer(
@@ -66,6 +108,7 @@ class VideoPlayerFragment : Fragment(R.layout.fragment_video_player) {
             return videoPlayer
         }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
