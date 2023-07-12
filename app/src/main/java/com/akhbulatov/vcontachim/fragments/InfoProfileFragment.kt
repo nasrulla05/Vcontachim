662013b4ebb3384f89819cbe058c316a91cdbfd1
Nasrulla Akhbulatov <nasrulla.akhbulatov@gmail.com>
2023-07-12 13:00:11 +0300

    safety net2

--- app/src/main/java/com/akhbulatov/vcontachim/fragments/InfoProfileFragment.kt
+++ app/src/main/java/com/akhbulatov/vcontachim/fragments/InfoProfileFragment.kt
@@ -34,10 +34,9 @@ class InfoProfileFragment : Fragment(R.layout.fragment_info_profile) {
 
         viewModel.infoProfileLiveData.observe(viewLifecycleOwner) { user ->
 
-            if (user.online == 1L) {
-                binding!!.onlineOrOffline.setImageResource(R.drawable.group_11)
-            } else {
-                binding!!.onlineOrOffline.setImageResource(R.drawable.ic_android_black_24dp)
+            binding!!.onlineOrOffline.apply {
+                if (user.online == 1L) setImageResource(R.drawable.group_11)
+                 else setImageResource(R.drawable.ic_android_black_24dp)
             }
 
             if (user.verified == 1L) {
